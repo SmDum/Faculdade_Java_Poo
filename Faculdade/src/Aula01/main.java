00e8b8332f49aaf05312e1a1bee6b53c242ea628
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-08-19 21:44:41 -0300

    Aula01

---
+++ Faculdade/src/Aula01/main.java
@@ -0,0 +1,9 @@
+package Aula01;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("Texto impresso na saída padrão."); // Mais Comum
+        System.out.printf("Texto formatado: %s, %d anos.\n", "Whiskers", 3); // Não tão comum
+        
+    }
+}
