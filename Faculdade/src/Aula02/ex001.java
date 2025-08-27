4a2bb12b5e4ff78be989fba5ec5df3e34f221c26
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-08-26 21:35:11 -0300

    Aula02

--- Faculdade/src/Aula02/ex001.java
+++ Faculdade/src/Aula02/ex001.java
@@ -2,8 +2,7 @@ package Aula02;
 
 public class ex001 {
         public static void main(String[] args) {
-            double soma;
-            soma = 4.5 + 7.5 + 9;
+            double soma = 4.5 + 7.5 + 9;
             System.out.println("Média dos Valores: " + (soma/3));
         }
 }
--- Faculdade/src/Aula02/ex002.java
+++ Faculdade/src/Aula02/ex002.java
@@ -4,6 +4,6 @@ public class ex002 {
     public static void main(String[] args) {
         double quadrado = 350;
         double area = quadrado * quadrado;
-        System.out.println(area + 'm');
+        System.out.println(area);
     }    
 }
