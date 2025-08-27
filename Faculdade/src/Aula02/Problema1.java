63b334c0ed7a6384e4478c150d3fbee9a3e82485
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-08-26 21:56:39 -0300

    Aula02

---
+++ Faculdade/src/Aula02/Problema1.java
@@ -0,0 +1,23 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class Problema1 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String nome = "Joao";
+
+        System.out.println("Digite o ano atual: ");
+        int anoAtual = scanner.nextInt();
+
+        System.out.println("Digite o ano de nascimento de João ");
+        int anoNascimento = scanner.nextInt();
+        
+        int anos = anoAtual - anoNascimento;
+        int anos2024 = 2024 - anoNascimento;
+
+        System.out.println(nome + " tem " + anos + " e terá " + anos2024 + " em 2024.");
+
+    }
+}
