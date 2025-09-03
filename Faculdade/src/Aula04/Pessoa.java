10281fe11233b59c2e19f5371a89eae808274ab0
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-09-02 21:26:06 -0300

    Aula04

---
+++ Faculdade/src/Aula04/Pessoa.java
@@ -0,0 +1,10 @@
+package Aula04;
+public class Pessoa {
+    public String nome;
+    public int idade;
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
---
+++ Faculdade/src/Aula04/PessoaMain.java
@@ -0,0 +1,10 @@
+package Aula04;
+
+public class PessoaMain {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Joao";
+        p1.idade = 25;
+        p1.exibirDados();
+    }
+}
