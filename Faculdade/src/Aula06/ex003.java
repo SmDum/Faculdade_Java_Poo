1a9b7ee7a7fd65b043d495a7ad1f1c87c0dfa089
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-09-09 22:20:13 -0300

    Aula06

--- Faculdade/src/Aula06/ex003.java
+++ Faculdade/src/Aula06/ex003.java
@@ -1,5 +1,15 @@
 package Aula06;
 
 public class ex003 {
-    
+    public static void main(String[] args) {
+        String p1 = "Victor ";
+        String p2 = "Homossexubixa";
+
+        System.out.println("Antes da Concatenacao: ");
+        System.out.println(p1);
+        System.out.println(p2);
+
+        System.out.println("Depois da Concatenacao");
+        System.out.println(p1.concat(p2));
+    }
 }
