b3a58065bac475560947004e97a885298d131f3c
Samuel de Moraes Delgado <samueldemoraesdelgado2005@gmail.com>
2025-10-06 08:46:16 -0300

    Exercícios

---
+++ Faculdade/src/EstudoAC1/ex001/main.java
@@ -0,0 +1,11 @@
+package ex001;
+
+public class main {
+    public static void Main(String[] args) 
+    {
+        for(int i = 0; i < 10; i++) 
+        {
+            System.out.println(i+1);
+        }
+    }
+}
---
+++ Faculdade/src/EstudoAC1/ex002/main.java
@@ -0,0 +1,13 @@
+package ex002;
+
+public class main 
+{
+    public static void Main(String[] args)
+    {
+        System.out.println("Tabuada do 5:");
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
